f467ac8ee76caddcbb811a60bfd926d3b71e58e2
Gamze <umacgamze49@gmail.com>
2022-01-27 13:02:20 +0100

    first bar chart

--- observable/src/app/app.module.ts
+++ observable/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarComponent } from './component/bar/bar.component';
+import { PieComponent } from './component/pie/pie.component';
+import { ScatterComponent } from './component/scatter/scatter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarComponent,
+    PieComponent,
+    ScatterComponent
   ],
   imports: [
     BrowserModule,
